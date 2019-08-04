964a926b76b50bb9719b94f4f204c24aa8d577ad
Samuel Porto <samuelportodosantos@gmail.com>
2019-08-04 15:57:31 -0300

    Controle de debug foi implementado

--- assets/js/damas.js
+++ assets/js/damas.js
@@ -1,10 +1,3 @@
-function log (string, color = null) {
-    if (color == null) {
-        color = "#09f"   
-    }
-    console.log(`%c ${string}`, `color: white; background-color: ${color};`);
-}
-
 let damas = new Vue({
     el: "#damas",
     data: {
@@ -28,6 +21,7 @@ let damas = new Vue({
         },
         playerOfTime: null,
         enemyPiece: [],
+        debug: false
     },
     mounted (){
         this.prepareBoard();
@@ -36,9 +30,11 @@ let damas = new Vue({
     },
     methods: {
         playBackgroundSong () {
-            let bgaudio = new Audio('sounds/background.mp3');
-            bgaudio.loop = true;
-            // bgaudio.play();
+            // let bgaudio = new Audio('assets/sounds/background.mp3');
+            // bgaudio.loop = true;
+            // document.body.addEventListener("mousemove", function () {
+            //     bgaudio.play(); 
+            // })
         },
         prepareBoard () {
             this.board.map((row, ri)=>{
@@ -59,19 +55,19 @@ let damas = new Vue({
             });
         },
         startRandomPlayer () {
-            log('[SYSTEM] Escolhendo o jogador que vai iniciar a partida!', '#09f')
+            this.log('[SYSTEM] Escolhendo o jogador que vai iniciar a partida!');
             this.playerOfTime = 2;
             // this.playerOfTime = Math.floor(Math.random() * 2);
             this.playerOfTime = 2;
             setTimeout(()=>{
-                log('[SYSTEM] O jogador '+this.playerOfTime+' vai iniciar a partida!', '#09f');
+                this.log('[SYSTEM] O jogador '+this.playerOfTime+' vai iniciar a partida!');
             }, 1000);
         },
         clickPiece (player, row, column) {
             this.enemyPiece.row = null;
             this.enemyPiece.column = null;
             if (this.playerOfTime != player) {
-                console.log("%c [SYSTEM] Você não pode mover as peças do seu oponente!" ,"color: white; background-color: #09f;");
+                this.log("[SYSTEM] Você não pode mover as peças do seu oponente!");
                 return;
             }
             //Set the piece of time
@@ -103,12 +99,12 @@ let damas = new Vue({
             this.enemyPiece.map((enemy)=>{
 
                 if (column < enemy.column && this.pieceOfTime.column > enemy.column) {
-                    console.log("A peça esta a esquerda");
+                    this.log("A peça esta a esquerda");
                     this.board[enemy.row][enemy.column].player = null;
                 }
 
                 if (column > enemy.column && this.pieceOfTime.column < enemy.column) {
-                    console.log("A peça esta a direita");
+                    this.log("A peça esta a direita");
                     this.board[enemy.row][enemy.column].player = null;
                 }
                 this.players[!this.playerOfTime+1].junk++;
@@ -119,8 +115,8 @@ let damas = new Vue({
             this.playerOfTime = this.playerOfTime == 1 ? 2 : 1;
             this.clearMoveSlots();
         },
-        setPlayablePlace(row, column) {           
-
+        setPlayablePlace(row, column) {
+            
             if (this.board[row][column] != null) {
                 if (this.board[row][column].player != this.playerOfTime && this.board[row][column].player == null)  {
                     if (column == this.pieceOfTime.column + 1 || column == this.pieceOfTime.column - 1) {
@@ -138,38 +134,26 @@ let damas = new Vue({
 
                     this.enemyPiece.map((enemy) => {
 
-                        // console.log("Peça do inimigo: ", enemy);
-                        // console.log("Peça do jogador: ", this.pieceOfTime);
-
                         let coluna_atual_jogador = this.pieceOfTime.column;
                         let coluna_atual_inimigo = enemy.column;
 
-                        console.log("Coluna do inimigo", coluna_atual_inimigo);
-                        console.log("Coluna do jogador", coluna_atual_jogador);
-                        
-                        // De que lado está a peça do inimigo em relacao ao jogador?
-
-                        // Jogando de cima pra baixo
                         if (this.playerOfTime == 1) {
                             if (coluna_atual_inimigo < coluna_atual_jogador) {
-                                log('[SYSTEM] Peça do inimigo está a esquerda', 'green');
+                                this.log('[SYSTEM] Peça do inimigo está a esquerda', 'green');
                                 this.enemyPlayablePlaces(enemy, 'left', 'topdown');
                             } else {
-                                log('[SYSTEM] Peça do inimigo está a direita', 'green');
+                                this.log('[SYSTEM] Peça do inimigo está a direita', 'green');
                                 this.enemyPlayablePlaces(enemy, 'right', 'topdown');
                             }
-                        }
-                        // Jogando de baixo pra cima
-                        else {
+                        } else {
                             if (coluna_atual_inimigo < coluna_atual_jogador) {
-                                log('[SYSTEM] Peça do inimigo está a esquerda', 'purple');
+                                this.log('[SYSTEM] Peça do inimigo está a esquerda', 'purple');
                                 this.enemyPlayablePlaces(enemy, 'left', 'downtop');
                             } else {
-                                log('[SYSTEM] Peça do inimigo está a direita', 'purple');
+                                this.log('[SYSTEM] Peça do inimigo está a direita', 'purple');
                                 this.enemyPlayablePlaces(enemy, 'right', 'downtop');
                             }
                         }
-                        
 
                     });
 
@@ -193,15 +177,14 @@ let damas = new Vue({
             this.prepareBoard();
         },
         enemyPlayablePlaces (enemy, side, vertical_position ) {
-            // retorna um array com as coordenadas das posiveis jogadas em relacao a peca do inimigo
 
             let casa_horizontal;
             let casa_vertical;
 
             if (side == 'left') {
-                log('[LEFT SIDE]', 'orange');
+                this.log('[LEFT SIDE]', 'orange');
 
-                log(`enemy row ${enemy.row} | enemy column ${enemy.column}`);
+                this.log(`enemy row ${enemy.row} | enemy column ${enemy.column}`);
                 
                 // posivel casa a esquerda do inimigo
                 casa_horizontal = enemy.column - 1;
@@ -214,23 +197,23 @@ let damas = new Vue({
 
                 // Verifica se as posicoes são validas
                 if (casa_vertical < 0 || casa_vertical > 7) {
-                    log(`Posicao vertical ${casa_vertical} não é válida`, 'red');
+                    this.log(`Posicao vertical ${casa_vertical} não é válida`, 'red');
                     return false;
                 }
                 if (casa_horizontal < 0 || casa_horizontal > 7) {
-                    log(`Posicao horizontal ${casa_horizontal} não é válida`, 'lightcoral');
+                    this.log(`Posicao horizontal ${casa_horizontal} não é válida`, 'lightcoral');
                     return false;
                 }
                 if (this.board[casa_vertical][casa_horizontal].player != null) {
-                    log(`[SYSTEM] O inimimgo não pode ser abatido! v:${casa_vertical} h:${casa_horizontal}`, 'red');
+                    this.log(`[SYSTEM] O inimimgo não pode ser abatido! v:${casa_vertical} h:${casa_horizontal}`, 'red');
                     return false;
                 }
                 this.board[casa_vertical][casa_horizontal].player = 3;
                 
             } else if (side == 'right') {
-                log('[RIGHT SIDE]', 'orange');
+                this.log('[RIGHT SIDE]', 'orange');
                 
-                log(`enemy row ${enemy.row} | enemy column ${enemy.column}`);
+                this.log(`enemy row ${enemy.row} | enemy column ${enemy.column}`);
                 
                 // posivel casa a esquerda do inimigo
                 casa_horizontal = enemy.column + 1;
@@ -243,19 +226,31 @@ let damas = new Vue({
 
                 // Verifica se as posicoes são validas
                 if (casa_vertical < 0 || casa_vertical > 7) {
-                    log(`Posicao vertical ${casa_vertical} não é válida`, 'red');
+                    this.log(`Posicao vertical ${casa_vertical} não é válida`, 'red');
                     return false;
                 }
                 if (casa_horizontal < 0 || casa_horizontal > 7) {
-                    log(`Posicao horizontal ${casa_horizontal} não é válida`, 'lightcoral');
+                    this.log(`Posicao horizontal ${casa_horizontal} não é válida`, 'lightcoral');
                     return false;
                 }
                 if (this.board[casa_vertical][casa_horizontal].player != null) {
-                    log(`[SYSTEM] O inimimgo não pode ser abatido! v:${casa_vertical} h:${casa_horizontal}`, 'red');
+                    this.log(`[SYSTEM] O inimimgo não pode ser abatido! v:${casa_vertical} h:${casa_horizontal}`, 'red');
                     return false;
                 }
                 this.board[casa_vertical][casa_horizontal].player = 3;
             }
+        },
+        displayDebug () {
+            this.debug = !this.debug;
+        },
+        log (string, color = null) {
+            if (this.debug != true) {
+                return false;
+            }
+            if (color == null) {
+                color = "#09f"   
+            }
+            console.log(`%c ${string}`, `color: white; background-color: ${color};`);
         }
         
     }
